1d36a3ba230f43558a947e608ebaaa3a35095a0c
avetyan-gayane <avetyan.gayane.01@gmail.com>
2023-07-04 00:09:44 +0400

    task 3_5

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    void divide(float toBeDivided) {
+        float res = toBeDivided / 5;
+        System.out.println(res);
     }
 }
